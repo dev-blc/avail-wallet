1530b7c5f3a6d94996e1b43655bc07aa4e4129d6
Zk <zklim95@hotmail.com>
2024-04-23 19:10:33 +0800

    feat: built params for sync_transaction and test working

--- src-tauri/src/services/record_handling/records_new.rs
+++ src-tauri/src/services/record_handling/records_new.rs
@@ -1,13 +1,30 @@
-use avail_common::errors::{AvailError, AvailResult};
+use avail_common::errors::AvailResult;
+use chrono::{DateTime, Local};
 use crate::services::local_storage::session::view::VIEWSESSION;
 use snarkvm::prelude::{
-	Network, Testnet3, ToField, FromStr, ViewKey, Parser, Field, Group, Scalar
+	Network, Testnet3, ToField, FromStr, Parser, Field, Group, Scalar, Transaction, Serialize
 };
+use snarkvm::ledger::transactions::ConfirmedTransaction;
+use snarkvm::synthesizer::program::FinalizeOperation;
 use serde_json::Value;
 use tauri_plugin_http::reqwest::Client;
-use availx_lib::services::record_handling::utils;
-use utils::sync_transaction;
+use super::utils::sync_transaction;
 
+/**
+ * SyncTxnParams struct
+ *
+ * A struct that holds the parameters for the sync transaction.
+ *
+ * transaction: The transaction to sync.
+ * block_height: The height of the block.
+ * timestamp: The timestamp of the block.
+ */
+#[derive(Debug, Clone)]
+struct SyncTxnParams<N: Network> {
+	transaction: ConfirmedTransaction<N>,
+	block_height: u32,
+	timestamp: DateTime<Local>,
+}
 /**
  * LocalClient struct
  *
@@ -25,9 +42,7 @@ struct LocalClient {
 	network_id: String
 }
 
-/**
- * Implementation of LocalClient
- */
+/// Implementation of the LocalClient struct.
 impl LocalClient {
 	fn new(api_key: String, base_url: String, network_id: String) -> Self {
 		LocalClient {
@@ -155,8 +170,6 @@ fn is_owner_direct<N:Network>(
 fn owned_records_to_transitions<N: Network>(records: Vec<Value>) -> Vec<String> {
 	let mut transitions = Vec::new();
 	for record in records {
-		println!("record: {:?}\n", record);
-
 		// Get values from record and cast to primitive types
 		let (_, nonce_x) = Field::<N>::parse(record.get("nonce_x").unwrap().as_str().unwrap()).unwrap();
 		let (_, nonce_y) = Field::<N>::parse(record.get("nonce_y").unwrap().as_str().unwrap()).unwrap();
@@ -177,15 +190,78 @@ fn owned_records_to_transitions<N: Network>(records: Vec<Value>) -> Vec<String> 
 	transitions
 }
 
+/// Convert transaction to confirmed transaction.
+fn convert_to_confirmed_transaction<N: Network>(transaction: Value) -> ConfirmedTransaction<N> {
+	let mut finalize_operations: Vec<FinalizeOperation<N>> = Vec::new();
+
+	// Get finalizes from transaction
+	let finalizes = transaction.get("finalize").unwrap().as_array().unwrap();
+	for finalize in finalizes {
+		finalize_operations.push(FinalizeOperation::from_str(&finalize.to_string()).unwrap());
+	}
+
+	// Return as confirmed transaction
+	ConfirmedTransaction::accepted_execute(
+		transaction.get("index").unwrap().as_u64().unwrap() as u32,
+		Transaction::from_str(&transaction.get("transaction").unwrap().to_string()).unwrap(),
+		finalize_operations
+	).unwrap()
+}
+
+/// Add to sync transaction parameters.
+fn add_to_sync_txn_params<N: Network>(
+	block: &Value,
+	block_height: u32,
+	timestamp: i64,
+	transition: &str,
+	sync_txn_params: &mut Vec<SyncTxnParams<N>>
+) {
+	// Get transactions from block
+	let transactions = block.get("transactions").unwrap().as_array().unwrap();
+	let mut included_txn: Vec<Value> = Vec::new();
+
+	for txn in transactions {
+		// Get transitions from transaction
+		let local_transitions = txn
+			.get("transaction").unwrap()
+			.get("execution").unwrap()
+			.get("transitions").unwrap()
+			.as_array().unwrap();
+
+		for tsn in local_transitions {
+			let id = tsn
+				.get("id").unwrap()
+				.as_str().unwrap();
+
+			// Check if it's owned transition and if already included in transactions array
+			if (id == transition && !included_txn.contains(txn)) {
+				println!("Found new owned transition:\n{} == {}\n", id, transition);
+				included_txn.push(txn.clone());
+				sync_txn_params.push(
+					SyncTxnParams {
+						transaction: convert_to_confirmed_transaction::<N>(txn.clone()),
+						block_height,
+						timestamp: DateTime::from_timestamp(timestamp, 0).unwrap().with_timezone(&Local)
+					}
+				);
+				println!("Added to sync transaction parameters:\n{}\n", txn);
+			}
+		}
+	}
+}
+
 /**
  * Get owned transactions.
  * @param transitions The transitions to get the transactions from.
  * @param client The client for the API requests.
  * @return The owned transactions.
  */
-async fn get_owned_transactions(transitions: Vec<String>, client: &LocalClient) -> Vec<Value> {
-	let mut transactions: Vec<Value> = Vec::new();
-	let mut block_heights: Vec<u64> = Vec::new();
+async fn convert_to_sync_txn_params<N: Network>(
+	transitions: Vec<String>,
+	client: &LocalClient
+) -> Vec<SyncTxnParams<N>> {
+	let mut sync_txn_params: Vec<SyncTxnParams<N>> = Vec::new();
+
 	for transition in transitions {
 		// Get block
 		let transaction_id = client.get_transaction_id_from_transition(&transition).await;
@@ -196,87 +272,136 @@ async fn get_owned_transactions(transitions: Vec<String>, client: &LocalClient) 
 			.get("header").unwrap()
 			.get("metadata").unwrap()
 			.get("height").unwrap()
-			.as_u64().unwrap();
+			.as_u64().unwrap() as u32;
 
-		// Check if block already scanned
-		if (block_heights.contains(&block_height)) {
+		// Get timestamp
+		let timestamp = block
+			.get("header").unwrap()
+			.get("metadata").unwrap()
+			.get("timestamp").unwrap()
+			.as_i64().unwrap();
+
+		// Skip if block already scanned
+		let scanned = sync_txn_params.iter().any(|s| s.block_height == block_height);
+		if (scanned) {
 			continue;
-		} else {
-			block_heights.push(block_height);
 		}
-
-		// Get transactions from block
-		let local_transactions = block.get("transactions").unwrap().as_array().unwrap();
-		for txn in local_transactions {
-			// Get transitions from transaction
-			let local_transitions = txn
-				.get("transaction").unwrap()
-				.get("execution").unwrap()
-				.get("transitions").unwrap()
-				.as_array().unwrap();
-
-			for tsn in local_transitions {
-				let id = tsn
-					.get("id").unwrap()
-					.as_str().unwrap();
-
-				// Check if it's owned transition and if already included in transactions array
-				if (id == transition && !transactions.contains(&txn)) {
-					transactions.push(txn.clone());
-					println!("Found equal ID:\n{} == {}\n", id, transition);
-					println!("Pushed transaction:\n{}\n", txn);
-				}
-			}
-		}
+		add_to_sync_txn_params(&block, block_height, timestamp, &transition, &mut sync_txn_params);
 	}
-	transactions
+	sync_txn_params
 }
 
 /**
- * Get records from the API and convert them to transitions.
+ * Get records.
  * @param start The start block.
  * @param end The end block.
- * @return The result of the operation.
+ * @return The records.
  */
 pub async fn get_records_new<N: Network>(
     start: u32,
     end: u32
-) -> AvailResult<()> {
+) -> AvailResult<(Vec<Value>)> {
     // Prepare API client and get records
-    let mut api_key: String = "bcde0fb4-a4fa-4e84-affd-ab70b5e477db".to_string();
-	let mut client = LocalClient::new(
+	// ATTENTION: Different API and base_url to get records
+    let api_key: String = "bcde0fb4-a4fa-4e84-affd-ab70b5e477db".to_string();
+	let client = LocalClient::new(
 		api_key, 
 		"https://aleo-testnet3.dev.obscura.network".to_string(), 
 		"testnet3".to_string()
 	);
     let records = client.get_records(start, end).await;
+    Ok((records))
+}
 
+/**
+ * Get sync transaction parameters.
+ * @param records The records to get the sync transaction parameters from.
+ * @return The sync transaction parameters.
+ */
+pub async fn get_sync_txn_params<N: Network>(records: Vec<Value>) -> AvailResult<(Vec<(SyncTxnParams<N>)>)> {
 	// Get transitions from owned records
 	let transitions = owned_records_to_transitions::<N>(records);
 
-	// Different API key for getting transaction
-	api_key = env!("TESTNET_API_OBSCURA").to_string();
-	client.change_api_key(api_key);
-	client.change_base_url("https://aleo-testnet3.obscura.network".to_string());
+	// Prepare API client
+	let api_key = env!("TESTNET_API_OBSCURA").to_string();
+	let client = LocalClient::new(
+		api_key,
+		"https://aleo-testnet3.obscura.network".to_string(),
+		"testnet3".to_string()
+	);
 
-	// Get owned transactions
-	let transactions = get_owned_transactions(transitions, &client).await;
+	// Get sync transaction parameters
+	let sync_txn_params = convert_to_sync_txn_params(transitions, &client).await;
 
-    Ok(())
+	Ok((sync_txn_params))
 }
 
 #[cfg(test)]
 mod record_handling_tests {
+	use avail_common::models::encrypted_data::EncryptedData;
+	use crate::models::pointers::record::AvailRecord;
 	use super::*;
 
 	#[tokio::test]
-	async fn test_get_records_new() {
+	async fn test_get_records() {
+		type N = Testnet3;
+
+		let view_key = env!("VIEW_KEY");
+		VIEWSESSION.set_view_session(view_key).unwrap();
+		let current_block: u32 = 2087986;
+		let start: u32 = current_block - 100;
+		let records= get_records_new::<N>(start, current_block).await;
+
+		assert!(records.is_ok());
+	}
+
+	#[tokio::test]
+	async fn test_get_sync_txn_params() {
+		type N = Testnet3;
+
 		let view_key = env!("VIEW_KEY");
 		VIEWSESSION.set_view_session(view_key).unwrap();
 		let current_block: u32 = 2087986;
 		let start: u32 = current_block - 10;
-		let success: Result<(), AvailError> = get_records_new::<Testnet3>(start, current_block).await;
+		let records= get_records_new::<N>(start, current_block).await;
 
-		assert!(success.is_ok());
+		let sync_txn_params = get_sync_txn_params::<N>(records.unwrap()).await;
+
+		assert!(sync_txn_params.is_ok());
+
+		for i in sync_txn_params.unwrap() {
+			println!("SyncTxnParams: {:?}", i);
+		}
+	}
+
+	#[tokio::test]
+	async fn test_sync_transaction() {
+		type N = Testnet3;
+
+		let view_key = env!("VIEW_KEY");
+		VIEWSESSION.set_view_session(view_key).unwrap();
+		let current_block: u32 = 2087986;
+		let start: u32 = current_block - 10;
+		let records= get_records_new::<N>(start, current_block).await;
+
+		let sync_txn_params = get_sync_txn_params::<N>(records.unwrap()).await.unwrap();
+
+		let mut res: Vec<AvailResult<(
+			Option<EncryptedData>,
+			Vec<AvailRecord<N>>,
+			Vec<EncryptedData>,
+			bool,
+		)>> = Vec::new();
+		for params in sync_txn_params {
+			res.push(sync_transaction::<Testnet3>(
+				&params.transaction,
+				params.block_height,
+				params.timestamp,
+				None,
+				None
+			));
+		}
+
+		assert!(res[0].is_ok());
 	}
 }
